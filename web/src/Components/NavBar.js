e80d1d720ac01cdc787a62f5f4f7f1320fb6fc8f
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-10-06 01:28:39 -0300

    Criação e estilização da NavBar

--- web/src/Components/NavBar.js
+++ web/src/Components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import { MdShoppingCart, MdHome } from "react-icons/md";
 
 import "./NavBar.css"
 import marvelLogo from '../Assets/Marvel_Logo.png'
-import avatarImg from '../Assets/Marvel_Logo.png'
+
 
 const NavBar = () => {
 
@@ -22,24 +23,23 @@ const NavBar = () => {
   }
 
   return (
+    <div>
     <nav className='navBar'>
       <ul className='list'>
-        <li>
-          <img className='avatarImg' onClick={navNotFound} src={marvelLogo} alt='logo da marvel' />
+        <img className='logoImage' onClick={navNotFound} src={marvelLogo} alt='logo da marvel' />
+      </ul >
+    </nav>
+    <nav className='navButtons'>
+        <ul className='list'>
+      <li className='item'>
+          <p onClick={navNotFound}><MdHome/></p>
         </li>
         <li className='item'>
-          <p onClick={navNotFound}>Home</p>
-        </li>
-        <li className='item'>
-          <p onClick={navRegister}>HQ's</p>
-        </li>
-        <li className='item'>
-          <p onClick={navLogin}>Perfil</p>
+          <p onClick={navLogin}> <MdShoppingCart /></p>
         </li>
       </ul>
-      <img className='avatarImg' onClick={navNotFound} src={avatarImg} alt='foto do usuário' />
-
     </nav>
+    </div>
 
   )
 }
---
+++ web/src/Pages/Home/index.js
@@ -0,0 +1,16 @@
+import NavBar from '../../Components/NavBar'
+
+import './style.css'
+
+const HomePage = () => {
+  return (
+    <div className='main'>
+      <NavBar />
+      <main>
+        <p></p>
+      </main>
+    </div>
+  )
+}
+
+export default HomePage
