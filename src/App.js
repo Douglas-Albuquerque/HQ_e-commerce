c647ab2dae2022f1ca947819f377c53950884e21
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-10-11 12:44:29 -0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
-import HomePage from "./Pages/Home";
 import RoutePage from "./Router/index"
 
 function App() {
   return (
     <div>
       <RoutePage />
-=======
-import RoutePage from "./Router/index";
-
-
-function App() {
-
-  return (
-    <div>
-        <RoutePage />
->>>>>>> refactor-app
     </div>
   );
 }
