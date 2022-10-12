fd2f0d8760d0f0bc0d0581f9c29c54a224544283
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-10-12 01:27:21 -0300

    Finalização de sincronismo de API de Detail_Page

--- src/Components/Cards/RareCard.js
+++ src/Components/Cards/RareCard.js
@@ -2,23 +2,25 @@ import capaGen from "../../Assets/capaGenerica.png"
 import { useNavigate } from "react-router-dom";
 import { CardText, ContainerImg, ImgCard, LiRareCard, RareTicket, TextRare } from "./StylesCards"
 
-const RareCard = ({ title, imgUrl, price }) => {
+const RareCard = ({ id, title, imgUrl, price, pageCount, description }) => {
+
 
   let imageCover = ""
-  if (imgUrl[0] === undefined) {
-    imageCover = capaGen
-  } else {
-    imageCover = imgUrl[0].path + ".jpg"
-  }
-  if (price === 0) {
-    price = 0.99
-  }
+  let pricing = ""
+  let viewPageCount = ""
+  let descrip = ""
+  { (imgUrl[0] === undefined) ? imageCover = capaGen : imageCover = imgUrl[0].path + ".jpg" }
+  { (price === 0) ? pricing = 0.13 : pricing = price }
+  { (pageCount === undefined) ? viewPageCount = "unknown" : viewPageCount = pageCount }
+  { (description === "") ? descrip = "unknown" : descrip = description }
   const navigateDetail = useNavigate();
 
+  const array = [{ pricing, descrip, title }]
+
   const navDetail = () => {
-    navigateDetail('/detail')
+    navigateDetail(
+      `/detail/${id}`);
   }
-  
 
   return (
     <div>
@@ -27,11 +29,11 @@ const RareCard = ({ title, imgUrl, price }) => {
           <TextRare>Raro</TextRare>
         </RareTicket>
         <ContainerImg>
-          <ImgCard id="img" onClick={navDetail} src={imageCover} alt=""/>
+          <ImgCard id="img" onClick={navDetail} src={imageCover} alt="" />
         </ContainerImg>
         <CardText>
           <p>{title}</p>
-          <p>Price:${price}</p>
+          <p>Price:${pricing}</p>
         </CardText>
       </LiRareCard>
     </div>
